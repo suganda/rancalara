355c3c8b243313fdd78897fb7a866bb0e51d3205
Dadang <ahash30@gmail.com>
2024-06-08 14:10:06 +0700

    update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <a class="btn btn-primary float-right"
                        href="{{ route('users.create') }}">
-                        Add New
+                        Add New..
                     </a>
                 </div>
             </div>
